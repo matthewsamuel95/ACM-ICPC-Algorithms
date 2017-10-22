4f49121e4bb1fddbca6e552a48482f11ad0875ed
maxlefebvre <maxlefebvre@cmail.carleton.ca>
2017-10-22 14:59:03 -0400

    added HeapSort for java

---
+++ Sorting/HeapSort.java
@@ -0,0 +1,66 @@
+public class HeapSort {
+
+    public static int[] heapSort(int arr[]) {
+        int n = arr.length;
+        // Build heap (rearrange array)
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(arr, n, i);
+
+        // One by one extract an element from heap
+        for (int i=n-1; i>=0; i--) {
+            // Move current root to end
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            // call max heapify on the reduced heap
+            heapify(arr, i, 0);
+        }
+        return arr;
+    }
+    // To heapify a subtree rooted with node i which is
+    // an index in arr[]. n is size of heap
+    private static void heapify(int arr[], int n, int i) {
+        int largest = i;  // Initialize largest as root
+        int l = 2*i + 1;  // left = 2*i + 1
+        int r = 2*i + 2;  // right = 2*i + 2
+
+        // If left child is larger than root
+        if (l < n && arr[l] > arr[largest])
+            largest = l;
+
+        // If right child is larger than largest so far
+        if (r < n && arr[r] > arr[largest])
+            largest = r;
+
+        // If largest is not root
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            // Recursively heapify the affected sub-tree
+            heapify(arr, n, largest);
+        }
+    }
+
+    public static void main(String[] args) {
+        int array[] = {473, 78, 92, 5, 18, 7, 65, 89};
+        int sortedArray[];
+        System.out.println("Before: ");
+        int n = array.length;
+        for (int i = 0; i < n; ++i) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+
+        sortedArray = heapSort(array);
+
+        System.out.println("After: ");
+        n = sortedArray.length;
+        for (int i = 0; i < n; ++i) {
+            System.out.print(sortedArray[i] + " ");
+        }
+        System.out.println();
+    }
+}
