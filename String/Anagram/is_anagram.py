ebf46e5272b3ebd6aeb0a14cb298e4109f0a70e1
david <carew124@gmail.com>
2017-10-22 18:48:27 -0700

    Only looks at alphanumeric characters now

--- String/Anagram/is_anagram.py
+++ String/Anagram/is_anagram.py
@@ -13,15 +13,16 @@ def is_anagram(str1, str2):
 
 def get_alphabet(string):
     """Creates a dictionary containing a count of the number of times
-    each character in string is used."""
+    each alphanumeric character in string is used."""
 
     """E.g. get_alphabet("Hello") would return {'H':1, 'e':1, 'l':2, 'o':1}"""
     alphabet = dict()
     for c in string:
-        if c in alphabet:
-            alphabet[c] = alphabet[c] + 1
-        else:
-            alphabet[c] = 1
+        if c.isalnum():
+            if c in alphabet:
+                alphabet[c] = alphabet[c] + 1
+            else:
+                alphabet[c] = 1
 
     return alphabet
 
