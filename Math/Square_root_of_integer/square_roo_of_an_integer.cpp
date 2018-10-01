b1ac7f825a35f8affd696e9e649d677dd30ab5d8
Rishabh <rg081999@gmail.com>
2018-10-02 00:24:48 +0530

    square root of an integer

---
+++ Math/Square_root_of_integer/square_roo_of_an_integer.cpp
@@ -0,0 +1,24 @@
+int floorSqrt(int x) 
+{ 
+    // Base cases 
+    if (x == 0 || x == 1) 
+    return x; 
+  
+    // Staring from 1, try all numbers until 
+    // i*i is greater than or equal to x. 
+    int i = 1, result = 1; 
+    while (result <= x) 
+    { 
+      i++; 
+      result = i * i; 
+    } 
+    return i - 1; 
+} 
+  
+// Driver program 
+int main() 
+{ 
+    int x = 11; 
+    cout << floorSqrt(x) << endl; 
+    return 0; 
+} 
