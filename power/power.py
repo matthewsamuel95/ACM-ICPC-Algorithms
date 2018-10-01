e9a04ea81a9e1abb32eea6d067dc27d010d5d875
Ubaid <daniloubaid@gmail.com>
2018-10-01 13:58:13 -0700

    power.py
    Implement pow(x, n), which calculates x raised to the power n (x^n).
    without built in functions
    handles negative raised numbers

---
+++ power/power.py
@@ -0,0 +1,17 @@
+def power(x,n):
+  orig_n = None
+  c = 1
+  r = x
+  if n == 0:
+    return 1
+  if n < 0:
+    orig_n = n
+    n = orig_n + (-orig_n) + (-orig_n)
+  while c < n:
+    r = r * x
+    c += 1
+  if orig_n is None:
+    return r
+  else:
+    r1 = 1 / float(r)
+    return r1
