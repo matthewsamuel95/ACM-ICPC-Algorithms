cefedf623d4b2ce389f3ad8e0a85a31f6edba857
Anand Rai <kumar.anand.rai@gmail.com>
2018-10-02 01:47:24 +0530

    next power of 2

---
+++ nextPowerOf2.java
@@ -0,0 +1,25 @@
+package cls;
+
+import java.util.Scanner;
+
+public class nextPowerOf2 {
+	
+	public static int findP(int n) {
+		int p = 1;
+		while(p<n) {
+			p*=2;
+		}
+		
+		
+		return p;
+	}
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		System.out.println(findP(n));
+		scanner.close();
+	}
+
+}
