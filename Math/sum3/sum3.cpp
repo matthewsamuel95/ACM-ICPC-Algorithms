ff31f87c13d5b4dc368530bddd797efa2027cd5d
Darsh-k-Shah <43760615+Darsh-k-Shah@users.noreply.github.com>
2018-10-02 01:10:49 +0530

    Update sum3.cpp

--- Math/sum3/sum3.cpp
+++ Math/sum3/sum3.cpp
@@ -7,6 +7,10 @@ void main()
   //let the number whose sum you want be called sum
  
  int flag=0;
+  
+  //the initial and final conditions of these loops are to minimize the number of repeated iterations
+  //it is also to make sure that the sum for any triplet is not done more than once
+  
   for (int i=0;i<n-2;i++)
     for(int j=i+1;j<n-1;j++)
       for(int k=j+1;k<n;k++)
