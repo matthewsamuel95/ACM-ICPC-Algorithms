5acdac618eecee569df24b36b83e62d26f208245
Rohit Das <rohit.das950@gmail.com>
2018-10-02 11:44:10 +0530

    power_of_x.cpp: Removed bit manipulation
    Removed bit manipulation used to make division by 2 faster, as bit manipulation for negative exponents was going wrong.

--- Math/power_of_x/power_of_x.cpp
+++ Math/power_of_x/power_of_x.cpp
@@ -6,7 +6,7 @@ using namespace std;
 float power(float x, int n) { 
     float temp;
     if (n == 0) return 1;
-    temp = power(x,n >> 1);
+    temp = power(x,n / 2);
     if (n % 2 == 0) return temp * temp;
     else {
         if (n > 0) return x*temp*temp;
