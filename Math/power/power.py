0b4ea2660c3635550c3549f8674373f9121408f4
Tanuj Rohilla <33057694+TanujRohilla@users.noreply.github.com>
2018-10-02 23:12:21 +0530

    Update power.py

--- Math/power/power.py
+++ Math/power/power.py
@@ -1,3 +1,13 @@
-a=float(input("Enter number:"))
-b=float(input("Enter its power:"))
-print(pow(a,b))
+def power(base, powerRaised):
+    if (powerRaised != 0):
+        return (base*power(base, powerRaised-1))
+    else:
+        return 1;
+
+base=float(input("Enter base:"))
+powerRaised=float(input("Enter power raised:"))
+
+if ( powerRaised >=0 ):
+    print(power(base,powerRaised))
+else:
+    print(1/(power(base,powerRaised*-1)))
