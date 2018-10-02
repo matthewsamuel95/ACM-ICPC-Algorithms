2f346f137c04364c15e85163074a0f6809e129cd
Rohit <rohit.das950@gmail.com>
2018-10-02 10:50:57 +0530

    power_of_x.cpp: Added cpp file to find x^n
    Added cpp code to find powe(x,n) without using library functions.
    Solves issue #159.

---
+++ Math/power_of_x/power_of_x.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+float power(float x, int n) { 
+    float temp;
+    if (n == 0) return 1;
+    temp = power(x,n >> 1);
+    if (n % 2 == 0) return temp * temp;
+    else {
+        if (n > 0) return x*temp*temp;
+        else return (temp*temp) / x;
+    }
+}   
+  
+int main() {
+    float x;
+    int n;
+    cout<<"Enter x and n: "<<endl;
+    cin>>x>>n; 
+    printf("%f \n", power(x, n)); 
+    return 0; 
+} 
