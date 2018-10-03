211d477f3f3fddd45d40586d30a592735acbbee7
harshits <harshits@qubole.com>
2018-10-03 18:56:53 +0530

    adding changes

--- Math/queue_using_stack/queue_using_stack.cpp
+++ Math/queue_using_stack/queue_using_stack.cpp
@@ -1,5 +1,4 @@
-// CPP program to implement Queue using
-// two stacks with costly enQueue()
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -8,39 +7,33 @@ struct Queue {
 
 	void enQueue(int x)
 	{
-		// Move all elements from s1 to s2
 		while (!s1.empty()) {
 			s2.push(s1.top());
 			s1.pop();
 		}
 
-		// Push item into s1
 		s1.push(x);
 
-		// Push everything back to s1
 		while (!s2.empty()) {
 			s1.push(s2.top());
 			s2.pop();
 		}
 	}
 
-	// Dequeue an item from the queue
 	int deQueue()
 	{
-		// if first stack is empty
+
 		if (s1.empty()) {
 			cout << "Q is Empty";
 			exit(0);
 		}
 
-		// Return top of s1
 		int x = s1.top();
 		s1.pop();
 		return x;
 	}
 };
 
-// Driver code
 int main()
 {
 	Queue q;
