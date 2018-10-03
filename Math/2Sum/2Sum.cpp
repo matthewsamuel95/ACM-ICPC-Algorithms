7a74fa591895f760380c5b9b59788c742b82e43f
varuncoc <43823353+varuncoc@users.noreply.github.com>
2018-10-03 23:12:44 +0530

    made changes

--- Math/2Sum/2Sum.cpp
+++ Math/2Sum/2Sum.cpp
@@ -1,42 +1,52 @@
-#include<iostream>
-#include<vector>
-#include<unordered_map>
+#include <algorithm>
+#include <ctime>
+#include <iostream>
+#include <random>
+#include <vector>
 
-using namespace std;
-
-
-vector<int> twoSum(vector<int>& nums, int target) {
-
-	unordered_map<int,int> mp;
-	vector<int> result;
-
-	for(int i=0; i < nums.size(); i++){
-		if(mp.find(target-nums[i]) != mp.end()) {
-			result.push_back(mp[target-nums[i]]);
-			result.push_back(i);
-			break;
-		}
-		else
-			mp[nums[i]] = i;
-	}
-	return result;
+long long random_number()
+{
+    const long long min = -100'000'000'000;
+    const long long max =  100'000'000'000;
+    static std::mt19937_64 gen(std::time(nullptr));
+    static std::uniform_int_distribution<long long> dist(min, max);
+    return dist(gen);
 }
 
+bool is_sum_exist(const std::vector<long long>& data, long long x)
+{
+    auto j = std::lower_bound(data.begin(), data.end(), x)  -  data.begin();
+    decltype(j) i = 0;
+    if(j == data.size()) --j;
+    while (i < j)  {
+        long long sum = data[i]+data[j];
+             if (sum == x)  return true;
+        else if (sum >  x)  --j;
+        else                ++i;
+    }
+    return false;
+}
 
-int main() {
-	
-	vector<int> nums;
+int main()
+{
+    const std::size_t data_size = 1'000'000;
+    const std::size_t requests = 100000;
+    std::cin.sync_with_stdio(false); //In case I need to output actual numbers
 
-	nums.push_back(2);
-	nums.push_back(7);
-	nums.push_back(11);
-	nums.push_back(15);
+    //creating dataset
+    std::vector<long long> data(data_size);
+    std::generate_n(data.begin(), data.size(), random_number);
+    std::sort(data.begin(), data.end());
 
-	int target = 9;
+    //creating values we will search sum of
+    std::vector<long long> rand_values(requests);
+    std::generate_n(rand_values.begin(), rand_values.size(), random_number);
+    std::cout << "done" << std::endl;
 
-	vector<int> result = twoSum(nums,target);
-
-	cout << result[0] << " " << result[1] << endl;
-
-	return 0;
+    long long counter = 0;
+    for(auto t: rand_values) {
+        if(is_sum_exist(data, t))
+            ++counter;
+    }
+    std::cout << counter;
 }
