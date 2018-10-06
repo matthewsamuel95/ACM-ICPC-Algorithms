13f184e43ccf69a1973fedbeac0dd9f7bd7272a4
hbostann <hbostann@gmail.com>
2018-10-07 02:21:09 +0300

    Added comments to python implementation of BFS

--- BFS/bfs.py
+++ BFS/bfs.py
@@ -3,8 +3,11 @@ import collections
 
 def bfs(graph, root): 
     visited, queue = set(), collections.deque([root])
+    # While we still have nodes to visit
     while queue: 
         vertex = queue.popleft()
+        # Add neighbours of current node to queue if they
+        # haven't been visited yet and mark them as visited.
         for neighbour in graph[vertex]: 
             if neighbour not in visited: 
                 visited.add(neighbour) 
