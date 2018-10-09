93b1ee2352d1f6aca449b3769f54a8e699dfa59a
Jonas <jonas.fab@freenet.de>
2018-10-09 10:08:17 +0200

    add a few comments to shellsort.java

--- Sorting/ShellSort/java/ShellSort.java
+++ Sorting/ShellSort/java/ShellSort.java
@@ -3,9 +3,11 @@ public class ShellSort{
     public static int[] shellSort(int arr[]){
         int temp;
         int n = arr.length;
+        // Gap of size n/2 initially
         for(int i = n/2; i>0; i=i/2){
             for(int j = i; j<n; j++){
                 for(int k=j-i;k>=0;k=k-i){
+                    // break loop if is higher, if not switch
                     if(arr[k+i]>=arr[k]){
                         break;
                     }else{
@@ -30,8 +32,8 @@ public class ShellSort{
         }
         System.out.println();
 
-        sortedArray = shellSort(array);
-
+        // shell sort
+        sortedArray = shellSort(array);        
         System.out.println("After: ");
         n = sortedArray.length;
         for (int i = 0; i < n; ++i) {
@@ -39,5 +41,4 @@ public class ShellSort{
         }
         System.out.println();
     }
-
 }
