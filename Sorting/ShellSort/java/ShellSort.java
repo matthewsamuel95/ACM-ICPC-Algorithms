bf3c7c55e0196047be7b6625e7c0ddb7eeec92dc
Jonas <jonas.fab@freenet.de>
2018-10-09 09:41:15 +0200

    add ShellSort in java

---
+++ Sorting/ShellSort/java/ShellSort.java
@@ -0,0 +1,43 @@
+public class ShellSort{
+
+    public static int[] shellSort(int arr[]){
+        int temp;
+        int n = arr.length;
+        for(int i = n/2; i>0; i=i/2){
+            for(int j = i; j<n; j++){
+                for(int k=j-i;k>=0;k=k-i){
+                    if(arr[k+i]>=arr[k]){
+                        break;
+                    }else{
+                        temp = arr[k];
+                        arr[k] = arr[k+i];
+                        arr[k+i] = temp;
+                    }
+                }
+            }
+        }
+        return arr;
+    }
+
+
+    public static void main(String[] args) {
+        int array[] = {473, 78, 92, 5, 18, 7, 65, 89, 7};
+        int sortedArray[];
+        System.out.println("Before: ");
+        int n = array.length;
+        for (int i = 0; i < n; ++i) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+
+        sortedArray = shellSort(array);
+
+        System.out.println("After: ");
+        n = sortedArray.length;
+        for (int i = 0; i < n; ++i) {
+            System.out.print(sortedArray[i] + " ");
+        }
+        System.out.println();
+    }
+
+}
