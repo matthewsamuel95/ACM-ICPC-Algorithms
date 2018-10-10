a5493f5251e57f2b465257d48356638bce6da1c7
subhahu123 <34541684+subhahu123@users.noreply.github.com>
2018-10-10 14:10:24 +0530

    Update with_var.c

--- swapping/with_var.c
+++ swapping/with_var.c
@@ -5,11 +5,12 @@
 int main()
 {
     int a,b,temp ;
-    printf("Enter value for a and b variable") ;
+    printf("Enter value for a and b variable\n") ;
     scanf("%d",&a) ;   //user input for a variable 
     scanf("%d",&b) ;   //user input for b variable
     temp=a ;           // {
     a=b ;              //   swapping value of a and b
     b=temp ;           //            } 
-    printf("a=%d b=%d",a,b) ;
+  //after swapping
+    printf(" after swapping value of  a=%d   &   b=%d",a,b) ;
 }
