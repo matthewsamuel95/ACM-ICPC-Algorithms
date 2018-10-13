98fe4c72e772f0e7dc50d89489f938685f907ca5
Afrizal Fikri <laser.survivor@gmail.com>
2018-10-13 16:57:38 +0700

    Implement merge and split

--- Data Structures/Treap/treap.cpp
+++ Data Structures/Treap/treap.cpp
@@ -1,22 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef struct node{
+typedef struct node {
     int val,prior,size;
     struct node *l,*r;
 } node;
 
 typedef node* pnode;
 
-int sz(pnode t){
+int sz (pnode t) {
     return t ? t->size : 0;
 }
 
-pnode init(int val){
+void upd_sz (pnode t) {
+    if (t) {
+        t->size = sz(t->l) + 1 + sz(t->r);
+    }
+}
+
+void split (pnode t, pnode &l, pnode &r, int key) {
+    if (!t) {
+        l = r = NULL;
+    }
+    else if (t->val <= key) {
+        split(t->r, t->r, r, key);
+        l = t;
+    }
+    else {
+        split(t->l, l, t->l, key);
+        r = t;
+    }
+
+    upd_sz(t);
+}
+
+void merge (pnode &t, pnode l, pnode r) {
+    if (!l || !r) {
+        t = l?l:r;
+    }
+    else if(l->prior > r->prior) {
+        merge(l->r, l->r, r);
+        t = l;
+    }
+    else {
+        merge(r->l, l, r->l);
+        t = r;
+    }
+
+    upd_sz(t);
+}
+
+pnode init (int val) {
     pnode ret = (pnode)malloc(sizeof(node));
-    ret->val=val;
-    ret->size=1;
-    ret->prior=rand();
-    ret->l=ret->r=NULL;
+    ret->val = val;
+    ret->size = 1;
+    ret->prior = rand();
+    ret->l = ret->r = NULL;
     return ret;
 }
