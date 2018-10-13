dcebcb9334fb1781dd785c14c1206c3962aaab95
Afrizal Fikri <laser.survivor@gmail.com>
2018-10-13 17:22:12 +0700

    Add BST routines

--- Data Structures/Treap/treap.cpp
+++ Data Structures/Treap/treap.cpp
@@ -18,6 +18,14 @@ void upd_sz (pnode t) {
     }
 }
 
+pnode find (pnode t, int key) {
+    if (t == NULL || t->val == key) {
+        return t;
+    }
+
+    return find(t->val < key ? t->r : t->l, key);
+}
+
 void split (pnode t, pnode &l, pnode &r, int key) {
     if (!t) {
         l = r = NULL;
@@ -38,7 +46,7 @@ void merge (pnode &t, pnode l, pnode r) {
     if (!l || !r) {
         t = l?l:r;
     }
-    else if(l->prior > r->prior) {
+    else if (l->prior > r->prior) {
         merge(l->r, l->r, r);
         t = l;
     }
@@ -50,6 +58,34 @@ void merge (pnode &t, pnode l, pnode r) {
     upd_sz(t);
 }
 
+void insert (pnode &t, pnode it) {
+    if (!t) {
+        t = it;
+    }    
+    else if (it->prior > t->prior) {
+        split(t, it->l, it->r, it->val);
+        t = it;
+    }
+    else {
+        insert(t->val <= it->val ? t->r : t->l, it);
+    }
+
+    upd_sz(t);
+}
+
+void erase (pnode &t, int key) {
+    pnode p = find(t, key);
+    if (p == NULL) {
+        return;
+    }
+    
+    pnode temp = t;
+    merge(t, p->l, p->r);
+    free(temp);
+
+    upd_sz(t);
+}
+
 pnode init (int val) {
     pnode ret = (pnode)malloc(sizeof(node));
     ret->val = val;
@@ -57,4 +93,19 @@ pnode init (int val) {
     ret->prior = rand();
     ret->l = ret->r = NULL;
     return ret;
+}
+
+int main() {
+    pnode t = init(4);
+    insert(t, init(19));
+    insert(t, init(8));
+    insert(t, init(27));
+    insert(t, init(20));
+    insert(t, init(12));
+    insert(t, init(15));
+    insert(t, init(6));
+    insert(t, init(7));
+    insert(t, init(8));
+  
+    return 0;
 }
