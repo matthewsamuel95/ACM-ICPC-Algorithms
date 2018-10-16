c2a9351e225ff7025e62b3a2d351eb6f36b11e1b
Shakib Hossain <shakib609@gmail.com>
2018-10-16 23:44:35 +0600

    Update is_anagram.py to take both upper and lowercase letters

--- String/Anagram/is_anagram.py
+++ String/Anagram/is_anagram.py
@@ -1,6 +1,6 @@
 def is_anagram(a: str, b: str):
-    a, b = a.lower(), b.lower()
     count = [0 for _ in range(26)]
+    count2 = [0 for _ in range(26)]
     a_length, b_length = len(a), len(b)
 
     if a_length != b_length:
@@ -9,10 +9,14 @@ def is_anagram(a: str, b: str):
     for i in range(a_length):
         if 'a' <= a[i] <= 'z':
             count[ord(a[i]) - ord('a')] += 1
+        elif 'A' <= a[i] <= 'Z':
+            count2[ord(a[i]) - ord('A')] += 1
         if 'a' <= b[i] <= 'z':
             count[ord(b[i]) - ord('a')] -= 1
+        elif 'A' <= b[i] <= 'Z':
+            count2[ord(b[i]) - ord('A')] -= 1
 
-    if any(count):
+    if any(count) or any(count2):
         return False
 
     return True
