a31a8f6a78d747d026c689e399ce0688cb26a30d
Misco <kmtsandeepanie@gmail.com>
2018-10-17 23:51:10 +0530

    factorial

---
+++ Math/Factorial/Factorial.java
@@ -0,0 +1,15 @@
+public class Factorial {
+
+    public static void main(String[] args) {
+        int num = 6;
+        long factorial = multiplyNumbers(num);
+        System.out.println("Factorial of " + num + " = " + factorial);
+    }
+    public static long multiplyNumbers(int num)
+    {
+        if (num >= 1)
+            return num * multiplyNumbers(num - 1);
+        else
+            return 1;
+    }
+}
