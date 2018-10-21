9860631af15c179f2d1fb193f897b1554f4647fe
5ar5kovic <petar.petkovic@pmf.edu.rs>
2018-10-21 02:13:17 +0200

    rename solved

---
+++ Sorting/Bubble Sort/C++/Bubble_Sort_2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int A[],int n)
+{
+	int i,j;
+	int temp;
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n-i;j++)
+		{
+			if(A[j]>A[j+1])
+			{
+				temp=A[j];
+				A[j]=A[j+1];
+				A[j+1]=temp;
+			}
+		}
+	}
+}
+
+int main()
+{
+	int n;
+	cout<<"Enter the size of the array\n";
+	cin>>n;
+	int a[n];
+	cout<<"Enter the element\n";
+	for(int i=0;i<n;i++)
+	{
+		cin>>a[i];
+	}
+	bubbleSort(a,n);
+	cout<<"The Sorted array is :-";
+	for(int i=0;i<n;i++)
+	{
+		cout<<a[i]<<" ";
+	}
+	cout<<endl;
+	return 0;
+}
