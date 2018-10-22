de72047cb712f618499d0e6fc833c60059f1c479
Charliiee <Charliiee@users.noreply.github.com>
2018-10-22 15:53:03 -0300

    Improve algorithm

--- Math/Prime/python/prime.py
+++ Math/Prime/python/prime.py
@@ -3,9 +3,14 @@ import sys
 import math
 
 def is_prime(number):
-    if (number <= 1):
+    if number <= 1:
         return False
-    for i in range(2, 1 + int(math.sqrt(number))):
+    if number == 2:
+        return True
+    if number % 2 == 0:
+        return False
+
+    for i in range(3, 1 + int(math.sqrt(number)), 2):
         if (number % i) == 0:
             return False
     return True
