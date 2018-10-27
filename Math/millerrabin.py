9e8707069e11518e1fbbdc6e1ba6ecc13f6771c5
dubesar <dubeysarvesh5525@gmail.com>
2018-10-28 01:39:49 +0530

    added python code for miller rabin theorem

---
+++ Math/millerrabin.py
@@ -0,0 +1,17 @@
+def isPrime(n, k=5): # miller-rabin
+    from random import randint
+    if n < 2: return False
+    for p in [2,3,5,7,11,13,17,19,23,29]:
+        if n % p == 0: return n == p
+    s, d = 0, n-1
+    while d % 2 == 0:
+        s, d = s+1, d/2
+    for i in range(k):
+        x = pow(randint(2, n-1), d, n)
+        if x == 1 or x == n-1: continue
+        for r in range(1, s):
+            x = (x * x) % n
+            if x == 1: return False
+            if x == n-1: break
+        else: return False
+    return True
