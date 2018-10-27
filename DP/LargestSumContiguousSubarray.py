6993dfb016d28640aed5d30c58f9d0b839e4113f
jrathod9 <lewisburnard@gmail.com>
2018-10-28 01:28:50 +0530

    Kadane's Algorithm DP

---
+++ DP/LargestSumContiguousSubarray.py
@@ -0,0 +1,20 @@
+# Python program to find maximum contiguous subarray
+# Function to find the maximum contiguous subarray
+from sys import maxint
+def maxSubArraySum(a,size):
+
+    max_so_far = -maxint - 1
+    max_ending_here = 0
+
+    for i in range(0, size):
+        max_ending_here = max_ending_here + a[i]
+        if (max_so_far < max_ending_here):
+            max_so_far = max_ending_here
+
+        if max_ending_here < 0:
+            max_ending_here = 0
+    return max_so_far
+
+# Driver function to check the above function
+a = [-10, -23, -225, -40]
+print "Maximum contiguous sum is", maxSubArraySum(a,len(a)) 
