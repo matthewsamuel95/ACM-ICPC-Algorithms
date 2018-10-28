c0b7d8be637b196f1a8c598ef9d38a558356006c
abdelrhman akram <abdelrhmanakram16@gmail.com>
2018-10-28 13:07:48 +0200

    add extended_gcd

---
+++ Math/extended_gcd/extended_gcd.cpp
@@ -0,0 +1,12 @@
+int gcd(int a, int b, int & x, int & y) {
+    if (a == 0) {
+        x = 0;
+        y = 1;
+        return b;
+    }
+    int x1, y1;
+    int d = gcd(b % a, a, x1, y1);
+    x = y1 - (b / a) * x1;
+    y = x1;
+    return d;
+}
