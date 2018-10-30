c300fa31833d10cf7ecabee40350bceaaa50182a
161b191 <33463093+161b191@users.noreply.github.com>
2018-10-30 10:08:03 +0530

    sum of digits
    n is the total number of digits.
    Time Complexity  O(n)

---
+++ Math/sum_of_digits/sum_of digits.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner (System.in);
+		int n=sc.nextInt();
+		int r=0,  sum=0;
+		while(n>0)
+		{
+			r=n%10;
+			sum+=r;
+			n=n/10;
+		}
+		System.out.println("Sum of digits "+sum);
+	}
+}
