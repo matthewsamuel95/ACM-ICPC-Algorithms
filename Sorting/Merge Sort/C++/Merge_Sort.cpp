4758fe4a677471bb60608ddcda2f88c1d7af5c50
OmarKimo <omarkimo80@gmail.com>
2018-10-31 15:24:06 +0200

    update & sync

--- Sorting/Merge Sort/C++/Merge_Sort.cpp
+++ Sorting/Merge Sort/C++/Merge_Sort.cpp
@@ -1,54 +1,67 @@
-#include <bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
-#define pb push_back
-
-void merge(vector<int> &a, int l, int m, int h){
-	int n1 = m-l+1;
-	int n2 = h-m;
-	vector<int> t1,t2;
-	t1.resize(n1);
-	t2.resize(n2);
-	for(int i=0;i<n1;i++)t1[i]=a[l+i];
-	for(int i=0;i<n2;i++)t2[i]=a[m+1+i];
-
-	int i(0),j(0),k(l);
-	while(i<n1 && j<n2){
-		if(t1[i]<=t2[j])a[k++]=t1[i++];
-		else a[k++]=t2[j++];
+void merge(int A[], int inicio, int meio, int fim){
+	int i=0, j=0, k=0;
+	int temp[100000], p1, p2, tam;
+	int fim1=0, fim2=0;
+	tam = fim-inicio+1;
+	p1 = inicio;
+	p2=meio+1;
+	for(i=0;i<tam;i++){
+		if(!fim1 && !fim2){
+			if(A[p1]<A[p2]){
+				temp[i] = A[p1++];
+			}
+			else{
+				temp[i] = A[p2++];
+			}
+			if(p1>meio){
+				fim1 = 1;
+			}
+			if(p2>fim){
+				fim2 = 1;
+			}
+		}
+		else{
+			if(!fim1){
+				temp[i] = A[p1++];
+			}
+			else{
+				temp[i] = A[p2++];
+			}
+		}
 	}
-	while(i<n1){
-		a[k++]=t1[i++];
-	}
-	while(j<n2){
-		a[k++]=t2[j++];
+	for(j=0,k=inicio;j<tam;j++,k++){
+		A[k] = temp[j];
 	}
 }
 
-void mergeSort(vector<int> &a, int l, int h){	
-	if(l<h){
-		int m = l+(h-l)/2;
-		mergeSort(a,l,m);
-		mergeSort(a,m+1,h);
-		merge(a,l,m,h);
+void merge_sort(int A[], int inicio, int fim){
+	int meio;
+	if(inicio<fim){
+		meio = (inicio+fim)/2;
+		merge_sort(A,inicio,meio);
+		merge_sort(A,meio+1,fim);
+		merge(A,inicio,meio,fim);
 	}
-
 }
 
-void print(vector<int> &a, int n){
-	for(int i=0;i<n;i++){
-		cout<<a[i]<<" ";
-	}cout<<endl;
-}
-int main(){
-	srand(time(0));
-	vector<int> a;
-	int n=10;
-	for(int i=0;i<n;i++){
-		a.pb(rand()%1001);
+int main() {
+	ios::sync_with_stdio(0);
+    	cin.tie(0);
+    
+	int vet[100000], vetn=0;
+	cin >> vetn;
+	int i;
+	for(i=0;i<vetn;i++){
+		cin >> vet[i];
 	}
-	print(a,n);
-	mergeSort(a,0,n-1);
-	print(a,n);
-
+	
+	//---(COLOCAR UMA FUNCAO SORT NESSE LUGAR)----//
+	
+	for(i=0;i<vetn;i++){
+		cout << vet[i] << " ";
+	}
+	return 0;
 }
