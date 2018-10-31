d2a4a80e34aa988e352ed1b46293ac68fe380a11
suporte <micromarket.rj@gmail.com>
2018-10-30 21:40:45 -0300

    code that ask a color and send a different reply to you

---
+++ casecolors.php
@@ -0,0 +1,17 @@
+<?php
+$favcolor = "red";
+
+switch ($favcolor) {
+    case "red":
+        echo "Your favorite color is red!";
+        break;
+    case "blue":
+        echo "Your favorite color is blue!";
+        break;
+    case "green":
+        echo "Your favorite color is green!";
+        break;
+    default:
+        echo "Your favorite color is neither red, blue, nor green!";
+}
+?>
