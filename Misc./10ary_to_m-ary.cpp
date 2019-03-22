dc0a79abf02f79cbf4dca1eaef6f388df746075b
Kushagra Nigam <kushagra1998@gmail.com>
2019-03-22 13:45:28 +0530

    Add files via upload

---
+++ Misc./10ary_to_m-ary.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+char a[16]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+
+string tenToM(int n, int m)
+{
+	int temp=n;
+	string result="";
+	while (temp!=0)
+	{
+		result=a[temp%m]+result;
+		temp/=m;
+	}
+return result;
+}
+
+int main()
+{
+	int n = 15;
+	int m = 8;
+	string str = tenToM(n,m);
+	cout<<str;
+}
