d7e2e77aef134d0b8c8db312ff02c8e28bccf097
glenprivacy <51202817+glenprivacy@users.noreply.github.com>
2019-07-26 11:09:45 +0800

    Calculation bug
    mid = (lower + (higher - lower)) / 2; is not working
    mid will just turn out to be higher / 2 in every loop

--- Search/BinarySearch/c++/bsearch.cpp
+++ Search/BinarySearch/c++/bsearch.cpp
@@ -15,7 +15,7 @@ bool binary_search(int number)
 
     do
     {
-        mid = (lower + (higher - lower)) / 2;
+        mid = (lower + higher) / 2;
         if (number == myarray[mid])
         {
             std::cout << "found at pos: " << mid + 1 << std::endl;
