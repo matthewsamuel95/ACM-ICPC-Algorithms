09f213b0543a292766cebf9e901a725812205a8a
Sakshi Maheshwari <sakshimaheshwari10@gmail.com>
2019-10-08 22:09:59 +0530

    Reverse_String_in_java

---
+++ String/Reverse String/reverse_string_in_java.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+import java.util.*;
+class Solution
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		String s=sc.next();
+		StringBuffer a = new StringBuffer(s);
+      		System.out.println(a.reverse());
+	}
+}
