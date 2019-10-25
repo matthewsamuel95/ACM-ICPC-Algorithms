11127d3faba2033dfbd47f2e64fd8ec67237f4bb
errorgorn <ashleykhoo2@gmail.com>
2019-10-25 16:55:55 +0800

    replaced functions to count number of bits to __builtin_popcount
    refer to https://gcc.gnu.org/onlinedocs/gcc/Other-Builtins.html for more details

--- BitManipulation/count_number_of_bits_to_be_flipped_to_convert_a_to_b/count_number_of_bits_to_be_flipped_to_convert_a_to_b.cpp
+++ BitManipulation/count_number_of_bits_to_be_flipped_to_convert_a_to_b/count_number_of_bits_to_be_flipped_to_convert_a_to_b.cpp
@@ -1,24 +1,13 @@
 #include<iostream>
 using namespace std;
 
-int countSetBits(int n) {
-    int setBits = 0;
-    while(n > 0){
-        if(n&1){
-            setBits++;
-        }
-        n>>=1;
-    }
-    return setBits;
-}
-
 int main(){
     int a,b;
     cin>>a>>b;
 
     // take the XOR of both the numbers and then count the set bits in that XOR
     int _xor = a^b;
-    int answer = countSetBits(_xor);
+    int answer = __builtin_popcount(_xor);
 
     cout<<"number of bits to be flipped "<<answer<<endl;
 
