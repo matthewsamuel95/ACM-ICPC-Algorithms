e6090bb1612b1e37618d3bd20b1214515952cb0c
Maria Luísa Mendes <72216924+lumendesp@users.noreply.github.com>
2020-10-13 22:24:19 -0300

    Update Bubble_Sort_2.cpp
    Added the option to choose the size of the array, choose the elements and the ordering.

--- Sorting/Bubble Sort/C++/Bubble_Sort_2.cpp
+++ Sorting/Bubble Sort/C++/Bubble_Sort_2.cpp
@@ -1,41 +1,65 @@
 #include <iostream>
 using namespace std;
 
-void bubbleSort(int A[],int n)
-{
-	int i,j;
-	int temp;
-	for(i=0;i<n;i++)
-	{
-		for(j=0;j<n-i;j++)
-		{
-			if(A[j]>A[j+1])
-			{
-				temp=A[j];
-				A[j]=A[j+1];
-				A[j+1]=temp;
-			}
-		}
-	}
+void bubbleSort(int array[], int size, int order){
+
+  if(order == 1){
+    for(int i=0; i < size-1; i++){
+      int flag = 0;
+
+      for(int j=0; j<size-1-i; j++){
+        if(array[j] > array[j+1]){
+          swap(array[j+1], array[j]);
+          flag = 1;
+        }
+      }
+      if(flag == 0){
+        break;
+      }
+    }
+  }
+
+  else if(order == 2){
+    for(int i=0; i < size-1; i++){
+      int flag = 0;
+
+      for(int j=0; j<size-1-i; j++){
+        if(array[j] < array[j+1]){
+          swap(array[j+1], array[j]);
+          flag = 1;
+        }
+      }
+      if(flag == 0){
+        break;
+      }
+    }
+  }
+
 }
 
-int main()
-{
-	int n;
-	cout<<"Enter the size of the array\n";
-	cin>>n;
-	int a[n];
-	cout<<"Enter the element\n";
-	for(int i=0;i<n;i++)
-	{
-		cin>>a[i];
-	}
-	bubbleSort(a,n);
-	cout<<"The Sorted array is :-";
-	for(int i=0;i<n;i++)
-	{
-		cout<<a[i]<<" ";
-	}
-	cout<<endl;
-	return 0;
+int main() {
+  int size;
+  int order;
+
+  cout << "Enter the size of the array:" << endl;
+	cin >> size;
+
+	int array[size];
+
+	cout << "Enter the elements of the array:" << endl;
+	for(int i = 0; i < size; i++){
+	  cin >> array[i];
+  }
+
+  cout << "What type of ordering do you want: \n 1 - Ascending \n 2 - Descending" << endl;
+  cin >> order;
+
+	bubbleSort(array, size, order);
+
+	cout << "The sorted array is:" <<endl;
+	for (int i = 0; i < size; i++) {
+		cout << array[i] << " ";
+  }
+
+  return 0;
 }
