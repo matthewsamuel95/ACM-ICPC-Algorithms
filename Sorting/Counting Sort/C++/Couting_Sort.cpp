8ae860ba6aceefcf8d46a77e5a903e6976be6cce
Jagadeesh <74336885+0-jagadeesh-0@users.noreply.github.com>
2022-07-04 13:07:03 +0530

    Updating Counting Sort with comments

--- Sorting/Counting Sort/C++/Couting_Sort.cpp
+++ Sorting/Counting Sort/C++/Couting_Sort.cpp
@@ -4,26 +4,78 @@
 
 using namespace std;
 
-void countingSort(vector<int> &v) {
-	int range = *max_element(begin(v), end(v));
-    vector<int> cnt(range + 1, 0);
-    for (int i = 0; i < (int)v.size(); ++i) {
-        ++cnt[v[i]];
+// Find the maximum element in the array.
+
+int findMax(int arr[], int n)
+{
+    int maxi = INT_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        maxi = arr[i] > maxi ? arr[i] : maxi;
     }
-    
-    for (int i = 0, j = -1; i <= range; ++i) {
-        for (int k = 0; k < cnt[i]; ++k) {
-            v[++j] = i;
-        }
+    return maxi;
+}
+
+void countingSort(int arr[], int n)
+{
+    int range = findMax(arr, n) + 1;
+
+    // Initialize an array to store the frequency of every element in the array.
+
+    int count[range] = {0};
+
+    // Counting the occurence of each element.
+
+    for (int i = 0; i < n; ++i)
+    {
+        count[arr[i]]++;
+    }
+
+    for (int i = 1; i <= range; i++)
+    {
+        count[i] += count[i - 1];
+    }
+
+    // Rotate the array left by one element.
+
+    for (int i = range; i > 0; i--)
+    {
+        count[i] = count[i - 1];
+    }
+    count[0] = 0;
+
+    // Declare an array to store the sorted elements.
+
+    int output[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        output[count[arr[i]]] = arr[i];
+        count[arr[i]]++;
+    }
+
+    // Arrage the output elements in original array.
+
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = output[i];
     }
 }
 
-int main() {
-	vector<int> v{10, 10, 20, 30, 12, 8, 9, 14, 13, 11};
-	countingSort(v);
-	for (int i = 0; i < (int)v.size(); ++i) {
-		cout << v[i] << " ";
-	}
-	cout << "\n";
-	return 0;
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    countingSort(arr, n);
+    for (int i = 0; i < n; ++i)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+    return 0;
 }
